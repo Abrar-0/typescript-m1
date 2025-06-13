f2230e5726ac844b63ccbd9c2487ed2fa072f55a
Abrar-0 <abrarzawad222@gmail.com>
2025-06-14 00:19:56 +0600

    objects and functions

--- module1/src/1.6.ts
+++ module1/src/1.6.ts
@@ -12,5 +12,19 @@ add (5, 10); // returns 15
 
 // Arrow function
 
-const addArrow = (num1: number, num2: number): number => num1 + num2;
+const addArrow = (num1: number, num2: number = 10): number => num1 + num2;
 addArrow(5, 10); // returns 15
+
+//object -> function -> method
+const poorUser = {
+    name: "Alice",
+    balance: 1000,
+    addBalance(balance: number): string {
+        return `My new balance is ${this.balance + balance}`;
+    }
+}
+
+//callback function
+
+const arr:number[] = [1, 2, 3, 4, 5];
+const double:number[] = arr.map((elem:number) :number => elem*elem)
