f7062a42df471686a8a2738ebf1de0169d470505
Abrar-0 <abrarzawad222@gmail.com>
2025-06-25 00:47:48 +0600

    interface and type asertion

--- module2/2.2.ts
+++ module2/2.2.ts
@@ -11,7 +11,9 @@
         age: number;
     }
 
-    interface
+    interface UserWithRole2 extends User2 { // interface can extend another interface and also a type -> extends User1
+        role: string;
+    }
 
     type UserWithRole = User1 & {role: string}; // intersection type
     const userR : UserWithRole = {
@@ -22,8 +24,6 @@
 
     type rollnumber = number; //interface can not be used to define primitive types like number, string, etc. It is used for objects, arrays, functions, etc.
     
-    
-
     const user1:  User1 = {
         name: "John Doe",
         age: 30
@@ -33,4 +33,24 @@
         name: "Jane Doe",
         age: 25
     }
+
+    type roll1 = number[]
+
+    const rollNumbers: roll1 = [101, 102, 103];
+
+    interface roll2 {
+        [index: number] : number; // index signature
+    }
+
+    const rollNumbers2: roll2 = [201, 202, 203];
+
+    type Add  = (a: number, b: number) => number; // type alias for function type
+
+    interface Add2 {
+        (a:number, b:number) : number
+    }
+
+    const add : Add = (a,b) => a+b
+
+    const add2: Add2 = (a, b) => a + b;
 }
