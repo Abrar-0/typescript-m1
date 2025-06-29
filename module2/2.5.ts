7b191cada9b19fabb52a63f174a6346200fad120
Abrar-0 <abrarzawad222@gmail.com>
2025-06-29 23:56:03 +0600

    generic functions

---
+++ module2/2.5.ts
@@ -0,0 +1,43 @@
+{
+    // geenric function
+
+    const createArray = (param: string): string [] =>{
+        return [param]
+    }
+
+    const createArrayWithGeneric = <boolean>(param: boolean): boolean [] =>{
+        return [param]
+    }
+
+    const res1 = createArray("Hello World");
+    const res2 = createArrayWithGeneric<boolean>(true);
+
+    type user = {
+        id: number,
+        name: string
+    }
+
+    const res3 = createArrayWithGeneric<user>({id:12, name:"John"})
+
+    const createArrayWithTuple = <T,Y>(param1: T,param2:Y): [T,Y] =>{
+        return [param1, param2]
+    }
+
+    const res4 = createArrayWithTuple<string,number>("Hello", 123);
+
+    interface User{ 
+        id:number;
+        name:string
+    }
+    const res5 = createArrayWithTuple<boolean,User>(true, {id: 1, name: "John"});
+
+    const addCourseToStudent = <T>(student: T) =>{
+        const course = "TypeScript Basics";
+
+        return {...student, course}
+    }
+
+    const student1 = addCourseToStudent({name: "Alice", age: 20});
+    const student2 = addCourseToStudent({name: "Bob", age: 22, grade: "A"});
+
+}
