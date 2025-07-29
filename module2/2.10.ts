a57841ae5788319d41f00add118326b61a7dcca9
Abrar-0 <abrarzawad222@gmail.com>
2025-07-30 00:21:25 +0600

    mapped type

--- module2/2.10.ts
+++ module2/2.10.ts
@@ -15,14 +15,29 @@
         width: number;
     }
 
+    type Height = AreaNum['height']; // lookup type
     // type AreaStr = {
     //     height: string;
     //     width: string;
     // }
     //keyof operator
-    type AreaStr = {
-        [key in keyof AreaNum]: string; // mapped type //keyof shows the keys of AreaNum
+    type AreaSt = {
+        [key in keyof AreaNum]: string; // mapped type
+    }
+    const area :AreaSt = {
+        height: '10',
+        width: '20'
+    }
+    
+    // T => {height: string, width: number}
+    // key => "height" | "width" 
+    type AreaStr <T> ={
+        [key in keyof T]: T[key]; // mapped type //keyof shows the keys of AreaNum
     }
 
+    const area1 : AreaStr<{height:string, width:number}> = {
+        height: '10',
+        width: 20
+    }
     
 }
